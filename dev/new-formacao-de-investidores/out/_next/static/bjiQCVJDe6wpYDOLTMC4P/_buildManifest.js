62c1a6530542a03f34310f2cadf83b56ffa5a3ef
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-06-17 18:03:36 -0300

    news sites

--- dev/new-formacao-de-investidores/out/_next/static/bjiQCVJDe6wpYDOLTMC4P/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4156e5d127a4b284.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
